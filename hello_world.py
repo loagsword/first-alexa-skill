d0eff93dd09b67c6caf15a8c9b5ce1f8b25aad04
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-01 22:08:49 -0400

    feature: added SessionEndRequestIntentHandler class

--- hello_world.py
+++ hello_world.py
@@ -68,3 +68,15 @@ class CancelAndStopIntentHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        # any cleanup logic goes here
+
+        return handler_input.response_builder.response
+
+
