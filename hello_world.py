374c168de0ef445f083014bce0709c5d475e3e30
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-01 22:09:58 -0400

    feature: added AllExceptionHandler class

--- hello_world.py
+++ hello_world.py
@@ -80,3 +80,21 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+
+class AllExceptionHandler(AbstractExceptionHandler):
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        # Log the exception in CloudWatch Logs
+        print(exception)
+
+        speech = "Sorry, I didn't get it. Can you please say it again?"
+        handler_input.response_builder.speak(speech).ask(speech)
+        return handler_input.response_builder.response
+
+
