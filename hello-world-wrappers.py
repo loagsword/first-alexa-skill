3c5c7188bcb42dd210bede1f0b31bcdc74eb9bec
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-03 10:23:16 -0400

    feature: replaced launch request handler with wrapper version

--- hello-world-wrappers.py
+++ hello-world-wrappers.py
@@ -5,25 +5,20 @@ from ask_sdk_core.skill_builder import SkillBuilder
 
 sb = SkillBuilder()
 
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
 
-class LaunchRequestHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("LaunchRequest")(handler_input)
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(
-            False)
-        return handler_input.response_builder.response
+    handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello World", speech_text)).set_should_end_session(
+        False)
+    return handler_input.response_builder.response
 
 
 class HelloWorldIntentHandler(AbstractRequestHandler):
