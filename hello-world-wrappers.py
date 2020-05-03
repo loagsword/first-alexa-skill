0df21379298d30552cd377de43599c259aa271aa
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-03 10:33:00 -0400

    refactor: replaced all_exception_handler implementation with wrapper version

--- hello-world-wrappers.py
+++ hello-world-wrappers.py
@@ -66,20 +66,15 @@ def session_ended_request_handler(handler_input):
 
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 
-class AllExceptionHandler(AbstractExceptionHandler):
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    # type: (HandlerInput, Exception) -> Response
+    # Log the exception in CloudWatch Logs
+    print(exception)
 
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        # Log the exception in CloudWatch Logs
-        print(exception)
-
-        speech = "Sorry, I didn't get it. Can you please say it again?"
-        handler_input.response_builder.speak(speech).ask(speech)
-        return handler_input.response_builder.response
+    speech = "Sorry, I didn't get it. Can you please say it again!!"
+    handler_input.response_builder.speak(speech).ask(speech)
+    return handler_input.response_builder.response
 
 
 sb.add_request_handler(LaunchRequestHandler())
